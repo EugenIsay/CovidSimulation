c0b777cd082cbb5f356d9ec006ae9395c68c2bf1
EugenIsay <zheka4311@gmail.com>
2025-02-04 15:41:49 +0300

    0.0.3

--- CovidSimulation/Human.cs
+++ CovidSimulation/Human.cs
@@ -9,31 +9,67 @@ namespace CovidSimulation
 {
     public class Human
     {
+        Random random = new Random();
+        public Human()
+        {
+            xCoordinate = 2;
+            yCoordinate = 3;
+            xDestination = 5;
+            yDestination = 7;
+            double deltaX = xDestination - xCoordinate;
+            double deltaY = yDestination - yCoordinate;
+            vectorX = deltaX / Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            vectorY = deltaY / Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            speed = 1;
+            
+        }
+
+        public int id;
+
         public string status { get; set; } = "Susceptible";
-        public double xCoordinate { get; set; } = new Random().Next(0, 500);
-        public double yCoordinate { get; set; } = new Random().Next(0, 500);
 
-        public double xDestination { get; set; } = new Random().Next(0, 500);
-        public double yDestination { get; set; } = new Random().Next(0, 500);
+        public double xCoordinate { get; set; } 
+        public double yCoordinate { get; set; }
 
-        public double speed { get; set; } = new Random().Next(1 , 10)/100;
-        
+        public double xDestination { get; set; } 
+        public double yDestination { get; set; }
+
+        public double speed { get; set; }
+
+        public double vectorX { get; set; }
+        public double vectorY { get; set; }
+
 
         public bool ReachedDestination
         {
-            get => xCoordinate == xDestination && yCoordinate == yDestination;
+            get
+            {
+                double deltaX = xDestination - xCoordinate;
+                double deltaY = yDestination - yCoordinate;
+                double vectorXNew = deltaX / Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+                double vectorYNew = deltaY / Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+                return (vectorXNew > 0) != (vectorX > 0) && (vectorYNew > 0) != (vectorY > 0);
+            }
         }
 
-        public Task Task()
+        public void Going()
         {
+
             if (!ReachedDestination)
             {
-
+                xCoordinate = xCoordinate + speed * vectorX;
+                yCoordinate = yCoordinate + speed * vectorY;
             }
-            return System.Threading.Tasks.Task.CompletedTask;
+            else
+            {
+                xDestination = random.Next(0, 500);
+                yDestination = random.Next(0, 500);
+                double deltaX = xDestination - xCoordinate;
+                double deltaY = yDestination - yCoordinate;
+                vectorX = deltaX / Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+                vectorY = deltaY / Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            }
         }
-
-
 
     }
 }
--- CovidSimulation/MainWindow.axaml.cs
+++ CovidSimulation/MainWindow.axaml.cs
@@ -3,30 +3,56 @@ using Avalonia.Controls.Shapes;
 using Avalonia.Media;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace CovidSimulation
 {
     public partial class MainWindow : Window
     {
+        List<Human> humans = new List<Human>();
+
         public MainWindow()
         {
             InitializeComponent();
             List<double> a = new List<double>() { 0, 6, 5, 7, 8, 9 };
-            ISeries[] series = new ISeries[] { new StackedAreaSeries<double> { Values = a} };
+            ISeries[] series = new ISeries[] { new StackedAreaSeries<double> { Values = a } };
             MainChart.Series = series;
-            Ellipse ellipse = new Ellipse() { Width = 5, Height = 5, Opacity = 1, Fill= Brush.Parse("Green") };
-            canvas.Children.Add(ellipse);
-            Ellipse ellipse2 = new Ellipse() { Width = 5, Height = 5, Opacity = 1, Fill = Brush.Parse("Red") };
-            canvas.Children.Add(ellipse2);
+            for (int i = 0; i < 1; i++)
+            {
+                humans.Add(new Human());
+                humans[i].id = i;
+                Ellipse ellipse = new Ellipse() { Width = 5, Height = 5, Opacity = 1, Fill = Brush.Parse("Green"), Tag = i };
+                if (humans[i].status == "Infected")
+                    ellipse.Fill = Brush.Parse("Red");
+                canvas.Children.Add(ellipse);
+                Canvas.SetLeft(ellipse, humans[i].xCoordinate);
+                Canvas.SetTop(ellipse, humans[i].yCoordinate);
+            }
+            humans[0].status = "Infected";
         }
 
         private void ClickStart(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            Task.WaitAll();
+            //Timer timer = new Timer(1000);
+            //timer.Elapsed += async (sender, e) => await ChangePosition();
+            //timer.Start();
+        }
+        private Task ChangePosition()
+        {
+            foreach (var human in humans)
+            {
+                human.Going();
+                Ellipse ellipse = (canvas.Children.FirstOrDefault(e => int.Parse(e.Tag.ToString()) == human.id) as Ellipse);
+                Canvas.SetLeft(ellipse, human.xCoordinate);
+                Canvas.SetTop(ellipse, human.yCoordinate);
+            }
+            return Task.CompletedTask;
         }
     }
 }
